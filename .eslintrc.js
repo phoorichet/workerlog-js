044c4b19776ebb6049fb5991dd35ae5c12438d2d
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2019-08-11 10:15:14 +0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {}
+}
--- example/test.js
+++ example/test.js
@@ -1,21 +1,19 @@
-"use strict";
-
-const Worker = require("../lib/workerlog.js");
+const Worker = require('../lib/workerlog.js')
 
 const dsn =
-  "https://cE0070046F0438F3c20F217FDc72F0281E188893@workerlog.dev/b350Fc2";
-const worker = new Worker(dsn);
+  'https://cE0070046F0438F3c20F217FDc72F0281E188893@workerlog.dev/b350Fc2'
+const worker = new Worker(dsn)
 
 const main = async () => {
   try {
-    const { status, data } = await worker.log("hello, worker", {
-      name: "worker",
-      module: "cloudflare"
-    });
-    console.log(status, data);
+    const { status, data } = await worker.log('hello, worker', {
+      name: 'worker',
+      module: 'cloudflare'
+    })
+    console.log(status, data)
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
-};
+}
 
-main();
+main()
